491484aa697b10605eb5afa96c017c4799001fea
strongfatfish <strongfatfish@gmail.com>
2020-08-19 03:16:30 +0800

    初始化播放器

---
+++ common/js/play.js
@@ -0,0 +1,53 @@
+import Storage from "./storage.js"
+import {
+	get_song_url,
+	get_song_detail
+} from "../../config/api.js"
+import {
+	toast
+} from "./utils.js"
+let Play = {}
+
+const get_player = () => getApp().globalData.play;
+Play.play = function() {
+	let player =get_player()
+	player.play(()=>{
+		console.log('当前音乐播放完毕');
+	},(e)=> {
+		console.log(e);
+	})
+}
+Play.start = function(song_id, cb) {
+	get_song_detail(song_id, (data) => {
+		console.log(data);
+		let song = data.songs[0];
+		let song_name = song.name;
+		let cover_image = song.al.picUrl;
+		let creators = song.ar;
+		let creator_str = '';
+		for (let creator of creators) {
+			creator_str = creator_str + creator.name;
+		}
+		get_song_url(song_id, (data) => {
+			let url = data.data[0].url;
+			if (!url) {
+				setTimeout(function() {
+					toast('none', '亲爱的, 不能播放这首歌哟~', 3000, false, 'bottom');
+				}, 10);
+				return;
+			}
+			let played = {
+				id: song_id,
+				url: url,
+				name: song_name,
+				creator: creator_str,
+				cover_image: cover_image,
+			}
+			get_player().src = url
+			get_player().play()
+			cb && cb(played)
+		});
+	})
+}
+
+export default Play;
---
+++ components/feiyu-play/feiyu-play.vue
@@ -0,0 +1,78 @@
+<template>
+	<view class=" play" v-show="played.cover_image">
+		<view class="container flex-row">
+			<view class="img flex-1">
+				<image :src="played.cover_image" mode="" style="width: 80rpx;height: 80rpx;" ></image>
+			</view>
+			<view class="flex-3">
+				
+			</view>
+			<view class="flex-1 flex-row row-around">
+				<view @click="play">
+					<feiyu-icon  :icon="icon" size=24></feiyu-icon>
+				</view>
+				<view class="">
+					<feiyu-icon icon="liebiao" size=26></feiyu-icon>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import Play from "../../common/js/play.js"
+export default {
+	data() {
+		return {};
+	},
+	computed: {
+		played() {
+			return this.$store.state.played;
+		},
+		playing() {
+			return this.$store.state.playing
+		},
+		icon() {
+			if(this.playing) {
+				return "Player"
+			}
+			return "Pause"
+		}
+	},
+	methods:{
+		play() {
+			Play.play()
+			this.$store.commit("SET_PLAYING")
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.play {
+	position: fixed;
+	bottom: 0rpx;
+	left: 0;
+	width: 100%;
+	height: 100rpx;
+	border-top-left-radius: 20rpx;
+	border-top-right-radius: 20rpx;
+	.img {
+		image {
+			border-radius: 50%;
+			animation: xuanzhuan 3s linear infinite;
+		}
+	}
+}
+@keyframes xuanzhuan {
+	0% {
+		transform: rotate(0deg);
+	}
+	50% {
+		transform: rotate(180deg);
+	}
+	100% {
+		transform: rotate(360deg);
+	}
+}
+</style>
